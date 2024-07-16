db62717f5c1251ffab98807aebe4dd6b15a8498e
Abinaya1207D <abi.dhanabalan12@gmail.com>
2024-07-17 03:03:08 +0530

    Add files via upload
    week1 day2

---
+++ day2/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="Google";
+		boolean beta=false;
+		int releaseyear=2008;
+		char shortcutkey='C';
+		
+	System.out.println("Version:"  +version);
+	System.out.println("Developer:" +developer);
+	System.out.println("Betatype:" +beta);
+	System.out.println("Year:" +releaseyear);
+	System.out.println("Key:" +shortcutkey);
+	
+	}
+}
